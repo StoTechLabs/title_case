4cbf143c905bec92666a9da476d4dcbf6b48cf28
Joshua Storey & Ginger Pearson <student@epicodus.com>
2014-07-22 16:06:12 -0700

    Second commit added spec for capitalizing letter for 1st word

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,4 +1,6 @@
-def title_case(word)
-  word = word.downcase
-    word
+def title_case(words)
+  words = words.downcase
+  capital_letter = words[0].upcase
+  new_word = capital_letter + words[1..-1]
+    new_word
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -3,7 +3,11 @@ require('title_case')
 
 describe('title_case') do
   it('should change the entire word to lower case') do
-    title_case("BoAt").should(eq('boat'))
+    title_case("BoAt").should(eq('Boat'))
+  end
+
+  it('capitalizes the first letter of each word') do
+    title_case("shoe").should(eq("Shoe"))
   end
 end
 
